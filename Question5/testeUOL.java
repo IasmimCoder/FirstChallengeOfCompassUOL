0c499087b3ae4ebd10bb230d244e937866572377
Iasmim Siqueira <iasmimsas@gmail.com>
2023-05-11 20:32:29 -0300

    finalizado

---
+++ Question5/testeUOL.java
@@ -0,0 +1,32 @@
+package Question5;
+
+public class testeUOL {
+    // COMANDOS UTILIZADO NO CLI DO MONGO-COMPASS
+
+    // use spring
+
+    /**
+     db.testeUOL.insertOne({
+        "nome": "Ana Silva",
+        "idade": 27,
+        "telefone": "+55 87 996262-1282",
+        "altura": 1.69,
+        "email": "ana.silva@gmail.com",
+        "cpf": "987.654.321-20",
+        "data_de_nascimento": "1983-01-14",
+        "address": {
+                "cep": "01234-567",
+                "rua": "Rua das Seu Zé",
+                "bairro": "Centro",
+                "complemento": "Casa",
+                "cidade": "Recife",
+                "estado": "PE",
+                "pais": "Brasil"
+            }
+        })
+     * 
+     * 
+     */
+
+
+}
