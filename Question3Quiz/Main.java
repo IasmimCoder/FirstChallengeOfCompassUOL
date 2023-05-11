9b6523666ddaab445fc2313887a30a0aee8e72ca
Iasmim Siqueira <iasmimsas@gmail.com>
2023-05-10 23:30:41 -0300

    modelo de negocio feito

---
+++ Question3Quiz/Main.java
@@ -0,0 +1,33 @@
+package Question3Quiz;
+
+import java.util.ArrayList;
+
+import Question3Quiz.model.LeitorDeQuestoes;
+import Question3Quiz.model.Question;
+import Question3Quiz.model.Sorteador;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Sorteador sorteador = new Sorteador(5);
+        // ArrayList<Question> questoesLidas = sorteador.getQuestoesSorteadas();
+        // System.out.println(questoesLidas.size());
+
+        // for(Question q: questoesLidas){
+        //     System.out.println("\n\n\nQuestaoooooooo\n");
+        //     System.out.println(q.getPergunta());
+            
+        //     String [] respostas = q.getOpcoesResposta();
+        //     for (String resposta : respostas) {
+        //         System.out.println(resposta);
+        //     }
+
+        // }
+
+        // LeitorDeQuestoes leitorDeQuestoes = new LeitorDeQuestoes();
+
+        // int qtdQuestoes = leitorDeQuestoes.getQuantidadeDeQuestoes();
+
+        // System.out.println(qtdQuestoes);
+    }
+}
