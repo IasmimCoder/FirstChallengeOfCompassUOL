a22a42bf5bbd79423674d8a3fdde62e868e4f266
Iasmim Siqueira <iasmimsas@gmail.com>
2023-05-11 12:38:34 -0300

    refatoracao feita

--- Question3Quiz/Main.java
+++ Question3Quiz/Main.java
@@ -13,28 +13,5 @@ public class Main {
 
         Apresentacao apresentacao = new Apresentacao();
         apresentacao.executar();
-
-
-        // Quiz quiz = new Quiz(5);
-        // int questoes = quiz.getQuestoes().length;
-        // System.out.println(questoes);
-     
-
-        // for(Question q: questoesLidas){
-        //     System.out.println("\n\n\nQuestaoooooooo\n");
-        //     System.out.println(q.getPergunta());
-            
-        //     String [] respostas = q.getOpcoesResposta();
-        //     for (String resposta : respostas) {
-        //         System.out.println(resposta);
-        //     }
-
-        // }
-
-        // LeitorDeQuestoes leitorDeQuestoes = new LeitorDeQuestoes();
-
-        // int qtdQuestoes = leitorDeQuestoes.getQuantidadeDeQuestoes();
-
-        // System.out.println(qtdQuestoes);
     }
 }
